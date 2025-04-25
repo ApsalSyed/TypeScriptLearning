81a7db916aefd468d712b1082b733969748b4be9
Apsal <apsal@swivl.tech>
2025-04-25 12:01:29 +0530

    adding react native paper to the project

--- index.js
+++ index.js
@@ -5,5 +5,13 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import React from 'react';
+import {Provider as PaperProvider} from 'react-native-paper';
 
-AppRegistry.registerComponent(appName, () => App);
+const Main = () => (
+  <PaperProvider>
+    <App />
+  </PaperProvider>
+);
+
+AppRegistry.registerComponent(appName, () => Main);
