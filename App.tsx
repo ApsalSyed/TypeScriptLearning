21b855ba04757a391cf57c7497e3b7fa4029e849
Apsal <apsal@swivl.tech>
2025-04-28 12:14:22 +0530

    wraping app with safe area view

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {View} from 'react-native';
 import HomePage from './src/page/home-page';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 const App: React.FC = () => {
   return (
-    <View style={{flex: 1}}>
+    <SafeAreaProvider>
       <HomePage />
-    </View>
+    </SafeAreaProvider>
   );
 };
 export default App;
